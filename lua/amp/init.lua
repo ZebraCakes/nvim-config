8ebd63dc8641a3c7ffbbec151dd797f919f6fe16
Adam Peterson <apeterson@allplan.com>
2025-06-30 12:17:20 -0500

    Clean whitespace on save

--- lua/amp/init.lua
+++ lua/amp/init.lua
@@ -1,4 +1,4 @@
 require("amp.remap")
 require("amp.options")
 require("amp.lazy")
-
+require("amp.autocommands")
--- lua/amp/lazy.lua
+++ lua/amp/lazy.lua
@@ -155,7 +155,7 @@ require("lazy").setup({
     },
     {
         "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "canary",
+        branch = "main",
         dependencies = {
           { "github/copilot.vim" },            -- or github/copilot.vim
           { "nvim-lua/plenary.nvim" },         -- for curl, log wrapper
